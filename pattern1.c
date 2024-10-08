d17d8d60c60f2e742518156cd85d55189015e5db
mamun-prodhan <mamunpr9@gmail.com>
2024-10-08 21:56:22 +0600

    pattern 2 implemented

--- pattern1.c
+++ pattern1.c
@@ -2,10 +2,10 @@
 
 int main(){
 
-    int n = 6;
+    int n = 5;
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
+    for(int i = n; i >= 1; i--){
+        for(int j = i; j >= 1; j--){
             printf("%d ", j);
         }
         printf("\n");
